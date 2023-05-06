1679c8b4dcc869f4a8484cd9dd9e40fa47040111
Frederick Alvarez <hk.fred.0@gmail.com>
2023-05-06 01:30:32 -0500

    added emojis

--- cli_client/src/help_cmd.rs
+++ cli_client/src/help_cmd.rs
@@ -1,8 +1,11 @@
+use colored::Colorize;
 use strum::IntoEnumIterator;
 use crate::cmds::Cmd;
 
 pub fn print_help() {
+    let rocket = emojis::get("🚀").unwrap();
     for cmd in Cmd::iter() {
-        println!("{:?}", cmd);
+        let cmd_str = cmd.to_string();
+        println!("{} {}", rocket.as_str(), cmd_str.green());
     }
 }
--- cli_client/src/terminal.rs
+++ cli_client/src/terminal.rs
@@ -1,11 +1,14 @@
 use std::io::{self, Write};
 use crate::cmds::Cmd;
 use crate::help_cmd::print_help;
+use colored::*;
 
 pub fn start_terminal() {
-    println!("Welcome to Blob Vader");
+    let bulb = emojis::get("💡").unwrap();
+    let bomb = emojis::get("💣").unwrap();
+    println!("{}{}", bulb.as_str(), "Welcome to Blob Vader".yellow());
     loop {
-        print!("blob> ");
+        print!("{} ", "blob>".cyan());
         io::stdout().flush().expect("Error flushing output buffer");
         let mut cmd = String::new();
         io::stdin().read_line(&mut cmd).expect("Error reading command");
@@ -31,7 +34,7 @@ pub fn start_terminal() {
                 print_help();
             }
             _ => {
-                println!("Unknown command {}", cmd);
+                println!("{} Unknown command '{}'", bomb.as_str(), cmd.red());
             }
         }
     }
