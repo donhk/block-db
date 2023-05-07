0d3467757d00461ff53353d6ee7c29c376882fde
Frederick Alvarez <hk.fred.0@gmail.com>
2023-05-07 10:44:48 -0500

    added ls command

--- cli_client/src/term/terminal.rs
+++ cli_client/src/term/terminal.rs
@@ -1,4 +1,3 @@
-use std::io::{self, Write};
 use crate::cli::commands::help_cmd::print_help;
 use crate::cli::commands::connect_cmd::connect;
 use crate::cli::commands::status_cmd::print_status;
@@ -6,6 +5,9 @@ use crate::cli::commands::cd_cmd::change_dir;
 use colored::*;
 use crate::utils::app_state::client_state;
 use std::env;
+use rustyline::{DefaultEditor};
+use rustyline::config::Configurer;
+use rustyline::error::ReadlineError;
 use crate::cli::cmds::Cmd;
 use crate::cli::commands::get_cmd::download_file;
 use crate::cli::commands::list_cmd::list;
@@ -19,42 +21,58 @@ pub fn init_app() {
 pub fn start_terminal() {
     let bulb = emojis::get("💡").unwrap();
     println!("{}{}", "Welcome to Blob Vader".yellow(), bulb.as_str());
+    let mut rl = DefaultEditor::new().unwrap();
+    rl.set_max_history_size(100).expect("Error setting history size");
     loop {
-        print!("{} ", "blob>".cyan());
-        io::stdout().flush().expect("Error flushing output buffer");
-        let mut cmd = String::new();
-        io::stdin().read_line(&mut cmd).expect("Error reading command");
-        let cmd = cmd.trim();
-        let cmd_lower = cmd.to_lowercase();
-        match cmd_lower {
-            s if s.starts_with(Cmd::EXIT.to_string().as_str()) => {
-                println!("Bye Bye!");
+        let readline = rl.readline(&*"blob>".cyan().to_string());
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).expect("error saving history");
+                let cmd = line.trim();
+                let cmd_lower = cmd.to_lowercase();
+                match cmd_lower {
+                    s if s.starts_with(Cmd::EXIT.to_string().as_str()) => {
+                        println!("Bye Bye!");
+                        break;
+                    }
+                    s if s.starts_with(Cmd::CONNECT.to_string().as_str()) => {
+                        connect(&cmd);
+                    }
+                    s if s.starts_with(Cmd::PUT.to_string().as_str()) => {
+                        upload_file(&cmd);
+                    }
+                    s if s.starts_with(Cmd::GET.to_string().as_str()) => {
+                        download_file(&cmd);
+                    }
+                    s if s.starts_with(Cmd::LS.to_string().as_str()) => {
+                        list();
+                    }
+                    s if s.starts_with(Cmd::HELP.to_string().as_str()) => {
+                        print_help();
+                    }
+                    s if s.starts_with(Cmd::STATUS.to_string().as_str()) => {
+                        print_status();
+                    }
+                    s if s.starts_with(Cmd::CD.to_string().as_str()) => {
+                        change_dir(&cmd);
+                    }
+                    _ => {
+                        let bomb = emojis::get("💣").unwrap();
+                        println!("{} Unknown command '{}'", bomb.as_str(), cmd.red());
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
                 break;
             }
-            s if s.starts_with(Cmd::CONNECT.to_string().as_str()) => {
-                connect(&cmd);
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
             }
-            s if s.starts_with(Cmd::PUT.to_string().as_str()) => {
-                upload_file(&cmd);
-            }
-            s if s.starts_with(Cmd::GET.to_string().as_str()) => {
-                download_file(&cmd);
-            }
-            s if s.starts_with(Cmd::LS.to_string().as_str()) => {
-                list();
-            }
-            s if s.starts_with(Cmd::HELP.to_string().as_str()) => {
-                print_help();
-            }
-            s if s.starts_with(Cmd::STATUS.to_string().as_str()) => {
-                print_status();
-            }
-            s if s.starts_with(Cmd::CD.to_string().as_str()) => {
-                change_dir(&cmd);
-            }
-            _ => {
-                let bomb = emojis::get("💣").unwrap();
-                println!("{} Unknown command '{}'", bomb.as_str(), cmd.red());
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
