119a6a80d01d59ed30304fd4976f68d9668174b2
Frederick Alvarez <hk.fred.0@gmail.com>
2023-05-07 15:55:29 -0500

    better client

--- cli_client/src/cli/commands/get_cmd.rs
+++ cli_client/src/cli/commands/get_cmd.rs
@@ -1,3 +1,46 @@
+use std::fs::File;
+use std::io::Write;
+use colored::Colorize;
+use crate::db::files_db::documents_db;
+use crate::network::file_transfer_client::FileTransferClient;
+use crate::network::{MessageReadRequest, MessageReadResponse};
+use crate::utils::app_state::client_state;
+
 pub fn download_file(raw_cmd: &str) {
-    println!("{}", raw_cmd)
+    let cmd_parts = raw_cmd.split_whitespace().collect::<Vec<&str>>();
+    if cmd_parts.len() != 2 || cmd_parts.len() != 3 {
+        println!("{} {}", "Invalid command: ".red(), "get <filename> <target>".blue());
+        return;
+    }
+    let source_file = *cmd_parts.get(1).unwrap();
+    let target_file = *cmd_parts.get(2).unwrap();
+    let doc = documents_db::get(source_file);
+    if doc.is_none() {
+        println!("{}", "Invalid document name '{}'".red());
+        return;
+    }
+    let doc = doc.unwrap();
+    let mut file = File::create(target_file).unwrap();
+    for chunk in doc.chunks {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            let response = get_data(chunk.clone()).await;
+            if !response.successful {
+                println!("Failed chunk: {}", chunk);
+                return;
+            }
+            file.write(&response.payload).expect("Error writing file");
+        });
+    }
+}
+
+async fn get_data(id: String) -> MessageReadResponse {
+    let server_url = client_state::get_client_conn().as_ref().unwrap();
+    let mut client = FileTransferClient::connect(server_url.to_string()).await.unwrap();
+    let request = tonic::Request::new(
+        MessageReadRequest {
+            message_id: id,
+        }
+    );
+    let output = client.receive_message(request).await;
+    output.unwrap().into_inner()
 }
