c3baae802e1e64bb3cb1a643c94dcef2eb0fb394
Frederick Alvarez <hk.fred.0@gmail.com>
2023-05-07 11:42:10 -0500

    better server

--- blob_server/src/main.rs
+++ blob_server/src/main.rs
@@ -19,6 +19,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let workspace = env::current_dir().unwrap().display().to_string();
     let storage = workspace + "/.storage";
+    std::fs::create_dir_all(storage.as_str())?;
     println!("listening on: http://{}", addr);
     println!("storage dir:  {}", storage);
     let future = start_server(addr, storage);
